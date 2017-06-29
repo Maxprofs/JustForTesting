6b32986cba7ebbe814fd4076594f009e99d0d1cd
III-ITC\Student EEIT9616 <lacerjoe@gmail.com>
2017-06-29 12:48:05 +0800

    no message

--- EEIT96/JDBC/JDBC_Sqlserver/basic/InsertDemo1.java
+++ EEIT96/JDBC/JDBC_Sqlserver/basic/InsertDemo1.java
@@ -1,42 +1,80 @@
-package com.iii.eeit96.joe;
+package com.iii.eeit96.lcpan;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 // Insert one employee
 public class InsertDemo1 {
 	public static void main(String[] args) {
 		Connection conn = null;
-
-//		LocalDate today = LocalDate.now();
-//		String input = today.toString();
-
-		try {
+		try {     
 			String connUrl = "jdbc:sqlserver://localhost:1433;databaseName=jdbc";
 			conn = DriverManager.getConnection(connUrl, "sa", "passw0rd");
-
+		/*	
+			java.util.Date today = new java.util.Date();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+			String insStmt = "INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?)";
+			PreparedStatement pstmt = conn.prepareStatement(insStmt);
+			pstmt.setInt(1, 1009);
+			pstmt.setString(2, "Jean Tsao");
+			pstmt.setString(3, sdf.format(today));
+			pstmt.setDouble(4, 55000);
+			pstmt.setInt(5, 100);
+			pstmt.setString(6, "senior engineer");
+			
+			java.util.Calendar today = java.util.Calendar.getInstance();
+			int year = today.get(Calendar.YEAR);
+			int month = today.get(Calendar.MONTH) + 1;
+			int day = today.get(Calendar.DATE);
+			String insStmt = "INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?)";
+			PreparedStatement pstmt = conn.prepareStatement(insStmt);
+			pstmt.setInt(1, 1009);
+			pstmt.setString(2, "Jean Tsao");
+			pstmt.setString(3, year + "/" + month + "/" + day);
+			pstmt.setDouble(4, 55000);
+			pstmt.setInt(5, 100);
+			pstmt.setString(6, "senior engineer");
+			
+		//	java.sql.Date today = new java.sql.Date(new java.util.Date().getTime());
+			java.sql.Date today = new java.sql.Date(System.currentTimeMillis());
+			String insStmt = "INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?)";
+			PreparedStatement pstmt = conn.prepareStatement(insStmt);
+			pstmt.setInt(1, 1009);
+			pstmt.setString(2, "Jean Tsao");
+			pstmt.setDate(3, today);
+			pstmt.setDouble(4, 55000);
+			pstmt.setInt(5, 100);
+			pstmt.setString(6, "senior engineer");
+			
+			// jdk 1.8
+			java.time.LocalDate today = java.time.LocalDate.now();
+			String insStmt = "INSERT INTO employee VALUES (?, ?, ?, ?, ?, ?)";
+			PreparedStatement pstmt = conn.prepareStatement(insStmt);
+			pstmt.setInt(1, 1009);
+			pstmt.setString(2, "Jean Tsao");
+			pstmt.setString(3, today.toString());
+			pstmt.setDouble(4, 55000);
+			pstmt.setInt(5, 100);
+			pstmt.setString(6, "senior engineer");
+		*/	
 			String insStmt = "INSERT INTO employee VALUES (?, ?, getdate(), ?, ?, ?)";
 			PreparedStatement pstmt = conn.prepareStatement(insStmt);
 			pstmt.setInt(1, 1009);
 			pstmt.setString(2, "Jean Tsao");
-//			pstmt.setString(3, input);
-
-			pstmt.setDouble(4, 55000);
-			pstmt.setInt(5, 100);
-			pstmt.setString(6, "senior engineer");
-
+			pstmt.setDouble(3, 55000);
+			pstmt.setInt(4, 100);
+			pstmt.setString(5, "senior engineer");
+			
 			int num = pstmt.executeUpdate();
 			System.out.println("insert count = " + num);
-
+			
 			pstmt = conn.prepareStatement("SELECT * FROM employee");
 			ResultSet rs = pstmt.executeQuery();
-
-			while (rs.next()) {
+			
+			while(rs.next()) {
 				System.out.print("name = " + rs.getString("ename") + ", ");
+				System.out.print("hiredate = " + rs.getString("hiredate") + ", ");
 				System.out.println("salary = " + rs.getDouble("salary"));
 			}
 		} catch (SQLException e) {
@@ -45,7 +83,7 @@ public class InsertDemo1 {
 			if (conn != null)
 				try {
 					conn.close();
-				} catch (SQLException e) {
+				} catch(SQLException e) { 
 					e.printStackTrace();
 				}
 		}
