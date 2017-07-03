67aae4146c74068d59c7cc7151a81c27587f120a
III-ITC\Student EEIT9616 <lacerjoe@gmail.com>
2017-07-03 13:18:04 +0800

    SQLIMP 作業 Final revision

--- EEIT96/SQL/SQL-IMP作業/Procedure2.java
+++ EEIT96/SQL/SQL-IMP作業/Procedure2.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class test4132 {
+public class Procedure2{
 
 	public static void main(String[] args) {
 		final String url = "jdbc:sqlserver://localhost:1433;databaseName=SQLIMP";
